ffada50f6e9975f3081e82b7dd981afa71b8ad01
Isaiah Skidmore <isaiahschardt0617@gmail.com>
2024-09-18 18:29:03 -0700

    added the content for the full About Me Page and changed the CSS for the word spacing. I will most likely change the styling later on as it's very basic right now

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,12 +1,24 @@
 export const AboutMe = () => {
-    return (
-        <div className="about-me">
-            <h2>About Me</h2>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, omnis eveniet sapiente quod tempora neque tenetur fugit possimus ullam, nulla voluptatem inventore ipsum illo quas. Sint laborum mollitia accusantium officia.
-            </p>
-        </div>
-    );
-}
+  return (
+    <div className="about-me">
+      <h2>About Me</h2>
+      <p>
+        Hello! I'm a full stack developer with a deep passion for coding and a
+        love for the outdoors. My journey into web development has been fueled
+        by a desire to create dynamic, user-friendly applications that make an
+        impact. From designing intuitive front-end interfaces to building robust
+        back-end systems, I enjoy the full spectrum of development and
+        continuously strive to improve my skills. When I'm not coding, you'll
+        often find me outdoors, hiking scenic trails, or overlanding/offroading,
+        or out on the golf course! I find that the balance between the structure
+        of development and the freedom of being outside helps keep me inspired
+        and grounded. With experience in a range of technologies, I aim to build
+        efficient, scalable, and creative solutions that meet user needs. I'm
+        always eager to collaborate on new projects, push the boundaries of what
+        I can create, and keep growing as a developer.
+      </p>
+    </div>
+  );
+};
 
 export default AboutMe;
--- src/components/HomeAboutMe.jsx
+++ src/components/HomeAboutMe.jsx
@@ -2,7 +2,7 @@
 // This component is the About Me section of the Home page. It contains a brief description of myself.
 export const HomeAboutMe = () => {
     return (
-        <div className="about-me">
+        <div className="homePageAboutMe">
             <h2>About Me</h2>
             <p>
             I'm a full stack developer with a passion for both coding and the outdoors. I enjoy building projects that blend creativity and functionality, and I’m always excited to take on new challenges in web development.            </p>

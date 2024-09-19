caf00bf199a4bf1146f1f890aab0618ecc00f01b
Isaiah Skidmore <isaiahschardt0617@gmail.com>
2024-09-18 19:55:05 -0700

    added the social icons for the footer and routed the hrefs to the proper links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,20 @@ import { Instagram } from "react-bootstrap-icons";
 export const Footer = () => {
     return (
         <div className="footer">
+            
+            <a href="https://github.com/IsaiahSkidmore" target="_blank" rel="noopener noreferrer">
             <button className="socials"><Github size={60} /></button>
+            </a>
+
+            <a href="https://www.linkedin.com/in/isaiah-skidmore-7b52331a0/" target="_blank" rel="noopener noreferrer">
+
             <button className="socials"> <Linkedin size={60} /></button>
+            </a>
+
+            <a href="https://www.instagram.com/iskidmorephotos/?igsh=OGQ5ZDc2ODk2ZA%3D%3D&utm_source=qr" target="_blank" rel="noopener noreferrer">
             <button className="socials"> <Instagram size={60} /></button>
+            </a>
+
         </div>
     );
 }

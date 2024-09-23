2b91d54355aff17543324abf99be18cc23e0ff39
Isaiah Skidmore <isaiahschardt0617@gmail.com>
2024-09-22 19:24:40 -0700

    added the logic for the email validation and the error saying the fields are required for the input form

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,30 +1,45 @@
-
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 
 function ContactMe() {
+  const [nameTouched, setNameTouched] = useState(false);
+  const [email, setEmail] = useState("");
+  const [emailTouched, setEmailTouched] = useState(false);
+  const [messageTouched, setMessageTouched] = useState(false);
+
+  const validateEmail = (email) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
   return (
     <section className='contact'>
-    <Form>
+      <Form>
         <div className="contact-container">
-        <Form.Group className="mb-3 contact-form" controlId="Full Name">
-        <Form.Label>Name</Form.Label>
-        <Form.Control type="input" placeholder="Full Name" />
-      </Form.Group>
-      </div>
-      <div className="contact-container">
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="name@example.com" />
-      </Form.Group>
-      </div>
+          <Form.Group className="mb-3 contact-form" controlId="Full Name">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="input" placeholder="Full Name" onBlur={() => setNameTouched(true)} />
+            {nameTouched && <div className="error">This field is required</div>}
+          </Form.Group>
+        </div>
         <div className="contact-container">
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Message</Form.Label>
-        <Form.Control as="textarea" placeholder="Message" rows={3} />
-      </Form.Group>
-      </div>
-      
-    </Form>
+          <Form.Group className="mb-3 contact-form" controlId="exampleForm.ControlInput1">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="name@example.com" 
+              onBlur={() => setEmailTouched(true)} 
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            {emailTouched && !validateEmail(email) && <div className="error">Please enter a valid email</div>}
+          </Form.Group>
+        </div>
+        <div className="contact-container">
+          <Form.Group className="mb-3 contact-form" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Message</Form.Label>
+            <Form.Control as="textarea" placeholder="Message" rows={3} onBlur={() => setMessageTouched(true)} />
+            {messageTouched && <div className="error">This field is required</div>}
+          </Form.Group>
+        </div>
+      </Form>
     </section>
   );
 }

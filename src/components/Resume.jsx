ae329a5f79cc341afebda8f84e001f32499737af
Isaiah Skidmore <isaiahschardt0617@gmail.com>
2024-09-24 22:20:15 -0700

    added the logic to download my resume when client clicks the button

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,30 +1,59 @@
+// import React from 'react';
+
+
+// export const Resume = () => {
+//     return (
+//     <section>
+
+
+
+//   <div className='button-container'>
+//     <a href="../assets/Isaiah_Skidmore_Resume.pdf" download>
+//     <button className='btn-resume'>Click To Download</button>
+//     </a>
+//     </div>
+    
+        
+//         <div className="list">
+//         <ul>
+//             <li>Full Stack Web Development</li>
+//             <li>JavaScript</li>
+//             <li>React & Node.JS</li>
+//             <li>RESTful API Integration</li>
+//             <li>Git and Version Control</li>
+//         </ul>
+//         </div>
+//     </section>
+//     );
+// };
+
+// export default Resume;
+
+
 import React from 'react';
 
 export const Resume = () => {
     return (
     <section>
-
-
-
-  <div className='button-container'>
-    <button className='btn-resume'>Click To Download</button>
-    </div>
-    
+        <div className='button-container'>
+            <a href="/Isaiah_Skidmore_Resume.pdf" download>
+                <button className='btn-resume'>Click To Download My Resume</button>
+            </a>
+        </div>
         
         <div className="list">
-        <ul>
-            <li>Full Stack Web Development</li>
-            <li>JavaScript</li>
-            <li>React & Node.JS</li>
-            <li>RESTful API Integration</li>
-            <li>Git and Version Control</li>
-        </ul>
+            <ul>
+                <li>Full Stack Web Development</li>
+                <li>JavaScript</li>
+                <li>React & Node.JS</li>
+                <li>RESTful API Integration</li>
+                <li>Git and Version Control</li>
+            </ul>
         </div>
     </section>
     );
 };
 
 export default Resume;
-
 
 
